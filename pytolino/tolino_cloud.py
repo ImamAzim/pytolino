86ee5de1a92afefe4ae19703309c94aeabf241fe
Imam Usmani <imam@e.email>
2023-10-21 22:22:42 +0200

    init client load settings

--- pytolino/tolino_cloud.py
+++ pytolino/tolino_cloud.py
@@ -1,1 +1,24 @@
 #!/usr/bin/env python3
+
+
+import os
+import configparser
+
+
+class PytolinoException(Exception)
+
+SERVERS_SETTINGS_FN = 'servers_settings.ini'
+SERVERS_SETTINGS_FILE_PATH = os.path.join(os.path.dirname(__file__), SERVERS_SETTINGS_FN)
+servers_settings = configparser.ConfigParser()
+servers_settings.read(SERVERS_SETTINGS_FILE_PATH)
+
+
+class Client(object):
+
+    """create a client to communicate with a tolino partner (login, etc..)"""
+
+    def __init__(self, server_name='www.buecher.de'):
+        try:
+            server_settings = servers_settings[server_name]
+        except KeyError:
+            raise PytolinoException
