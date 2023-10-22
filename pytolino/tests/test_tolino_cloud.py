8b5a228f1f38d72ca32ea84d7c85f0d259eac086
Imam Usmani <imam@e.email>
2023-10-22 22:13:58 +0200

    test no partner

--- pytolino/tests/test_tolino_cloud.py
+++ pytolino/tests/test_tolino_cloud.py
@@ -6,7 +6,14 @@ test all the tools in tolino cloud
 import unittest
 
 
+from pytolino.tolino_cloud import Client, PytolinoException
+
+
 class TestClient(unittest.TestCase):
 
     """all test concerning the Client class. """
 
+    def test_init_config(self):
+
+        with self.assertRaises(PytolinoException):
+            client = Client(server_name='this tolino partner does not exists')
