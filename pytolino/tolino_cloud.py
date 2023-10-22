397ac94e4ac1643e815e8ed9a0f19f7ed98bf4c7
Imam Usmani <imam@e.email>
2023-10-22 21:52:05 +0200

    fix ini filename

--- pytolino/tolino_cloud.py
+++ pytolino/tolino_cloud.py
@@ -5,13 +5,19 @@ import os
 import configparser
 
 
-class PytolinoException(Exception): pass
+class PytolinoException(Exception):
+    pass
 
 
 SERVERS_SETTINGS_FN = 'servers_settings.ini'
 SERVERS_SETTINGS_FILE_PATH = os.path.join(os.path.dirname(__file__), SERVERS_SETTINGS_FN)
 servers_settings = configparser.ConfigParser()
 servers_settings.read(SERVERS_SETTINGS_FILE_PATH)
+
+
+def main():
+    print(SERVERS_SETTINGS_FILE_PATH)
+    print(servers_settings.sections())
 
 
 class Client(object):
@@ -23,3 +29,7 @@ class Client(object):
             server_settings = servers_settings[server_name]
         except KeyError:
             raise PytolinoException(f'no partner {server_name} found')
+
+
+if __name__ == '__main__':
+    main()
