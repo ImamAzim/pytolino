aaac38e305adda3903bbc11de8e7655a972f73f5
Imam Usmani <imam@e.email>
2023-10-22 22:32:10 +0200

    test server settings

--- pytolino/tests/test_tolino_cloud.py
+++ pytolino/tests/test_tolino_cloud.py
@@ -13,7 +13,12 @@ class TestClient(unittest.TestCase):
 
     """all test concerning the Client class. """
 
-    def test_init_config(self):
+    def test_init_nopartner(self):
 
         with self.assertRaises(PytolinoException):
             client = Client(server_name='this tolino partner does not exists')
+
+    def test_init_partner_config(self):
+        client = Client()
+        n_settings = len(client.server_settings)
+        self.assertGreater(n_settings, 0) 
