d9cf0b68d477d9ecc22327be0f09141d4e1c5b2a
Imam Usmani <imam@e.email>
2023-10-29 21:26:53 +0100

    doc rm path

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -29,8 +29,3 @@ exclude_patterns = []
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here.
-import pathlib
-import sys
-sys.path.insert(0, pathlib.Path(__file__).parents[2].resolve().as_posix())
