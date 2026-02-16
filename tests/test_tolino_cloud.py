d0dc0e97059192f4f6416e70cd079c323eb2ec4d
Imam Usmani <imam@e.email>
2026-02-16 23:28:39 +0100

    revert to test name

--- tests/test_tolino_cloud.py
+++ tests/test_tolino_cloud.py
@@ -186,8 +186,8 @@ def login_test():
     client = Client()
     client.login(username, password)
     # print(client.refresh_token, client.hardware_id)
-    client.store_current_token('tolino_news')
-    client.get_new_token('tolino_news')
+    client.store_current_token('test')
+    client.get_new_token('test')
 
 
 if __name__ == '__main__':
