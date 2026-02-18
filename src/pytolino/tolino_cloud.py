afcaee3bcf7dc0e8d3e6b447f4f8edc1711153ae
Imam Usmani <imam@e.email>
2026-02-18 22:36:47 +0100

    fix header TE

--- src/pytolino/tolino_cloud.py
+++ src/pytolino/tolino_cloud.py
@@ -193,6 +193,7 @@ class Client(object):
                 'Sec-Fetch-Dest': 'empty',
                 'Sec-Fetch-Mode': 'cors',
                 'Sec-Fetch-Site': 'cross-site',
+                'TE': 'trailers',
                 'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:147.0) Gecko/20100101 Firefox/147.0',
                 }
         payload = {
