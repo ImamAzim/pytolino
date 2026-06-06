21e258e427bb00556432e681cb720474fb7d8f82
Imam Usmani <imam@e.email>
2026-06-06 22:16:03 +0200

    test not finished

--- tests/test_tolino_cloud.py
+++ tests/test_tolino_cloud.py
@@ -130,7 +130,6 @@ def mark_finished_test():
 def mark_not_finished_test():
     print("mark not finish test")
     vb = VarBox("pytolino")
-    ebook_id = vb.ebook_id
 
     username, password = get_test_credentials()
     client = Client(username)
@@ -139,14 +138,15 @@ def mark_not_finished_test():
     except PytolinoException as e:
         print(e)
     else:
-        try:
-            revision, patch_rev, patch = client.mark_book_as_not_finished(ebook_id)
-        except PytolinoException as e:
-            print(e)
-        else:
-            print(revision)
-            print(patch_rev)
-            print(patch)
+        for ebook_id in [vb.deliverable_id, vb.identifier]:
+            try:
+                revision, patch_rev, patch = client.mark_book_as_not_finished(ebook_id)
+            except PytolinoException as e:
+                print(e)
+            else:
+                print(revision)
+                print(patch_rev)
+                print(patch)
 
 
 def sync_test():
@@ -324,9 +324,9 @@ def get_test_credentials():
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
     # login_test()
-    mark_finished_test()
+    # mark_finished_test()
+    mark_not_finished_test()
     # sync_test()
-    # mark_not_finished_test()
     # sync_test()
     # upload_test()
     # download_test()
